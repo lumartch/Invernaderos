4746d683ac672fe266842bb5d9ed8c09a926e849
Lumart Chryssomallis <lumartch@gmail.com>
2018-04-16 09:57:58 -0500

    Pagina de cultivos creada

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CultivosPage } from '../cultivos/cultivos';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+	cultivosPage = CultivosPage;
   constructor(public navCtrl: NavController) {
 
+  }
+  cambiarCultivos(){
+  	this.navCtrl.push(this.cultivosPage);
   }
 
 }
